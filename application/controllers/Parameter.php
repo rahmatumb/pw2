6180a055dfda66205709bbdec13efb2259831749
rahmatumb <rahmat.oktavian@mercubuana.ac.id>
2020-03-09 15:36:44 +0700

    pertemuan1

--- application/controllers/Parameter.php
+++ application/controllers/Parameter.php
@@ -9,13 +9,13 @@ class Parameter extends CI_Controller {
 
 	public function get() {
 		$output = array(
-						'judul' => 'Parameter Get',
-						'link' => array(
-										'berhenti' => 'red',
-										'pelan' => 'yellow',
-										'jalan' => 'green'
-									)
-					);
+					'judul' => 'Parameter Get',
+					'link' => array(
+								'berhenti' => 'red',
+								'pelan' => 'yellow',
+								'jalan' => 'green'
+							)
+				);
 
 		$this->load->view('parameter_get', $output);
 	}
@@ -24,9 +24,9 @@ class Parameter extends CI_Controller {
 		$tanda = $this->uri->segment(3);
 
 		$output = array(
-						'judul' => 'Parameter Get Result',
-						'tanda' => $tanda
-					);
+					'judul' => 'Parameter Get Result',
+					'tanda' => $tanda
+				);
 
 		$this->load->view('parameter_get_result', $output);
 	}
@@ -43,12 +43,12 @@ class Parameter extends CI_Controller {
 		$umur = $this->input->post('umur');
 
 		$output = array(
-						'judul' => 'Parameter Post Result',
-						'post' => array(
-								'nama' => $nama,
-								'umur' => $umur
-							)
-					);
+					'judul' => 'Parameter Post Result',
+					'post' => array(
+						'nama' => $nama,
+						'umur' => $umur
+					)
+				);
 		$this->load->view('parameter_post_result', $output);
 	}
 }
