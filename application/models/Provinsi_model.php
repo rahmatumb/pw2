5eeb90d320e68b2c0f11e40b8c01be69baa22569
rahmatumb <rahmat.oktavian@mercubuana.ac.id>
2020-03-17 21:55:03 +0700

    pertemuan 3

--- application/models/Provinsi_model.php
+++ application/models/Provinsi_model.php
@@ -5,15 +5,19 @@ class Provinsi_model extends CI_Model {
 
 	//function read berfungsi mengambil/read data dari table provinsi di database
 	public function read() {
+
+		//sql read
 		$this->db->select('*');
 		$this->db->from('provinsi');
-
 		$query = $this->db->get();
+
+		//mengirim data ke controller
         return $query->result_array();
 	}
 
 	//function insert berfungsi menyimpan/create data ke table provinsi di database
 	public function insert($data) {
+		//$data = data yang dikirim dari controller
 		return $this->db->insert('provinsi', $data);
 	}
 }
