b1220c38c2d05e6f4edce740e992385e5db1535f
rahmatoktavian <rahmatoktavian@gmail.com>
2021-09-21 07:24:03 +0700

    initapp

--- create_insert_table_kota.sql
+++ create_insert_table_kota.sql
@@ -7,10 +7,3 @@ CREATE TABLE `kota` (
   KEY `id_provinsi` (`id_provinsi`),
   CONSTRAINT `kota_ibfk_1` FOREIGN KEY (`id_provinsi`) REFERENCES `provinsi` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-INSERT INTO `kota` (`id`, `id_provinsi`, `nama`, `penduduk`) VALUES
-(1,	1,	'Jakarta Barat',	8000000),
-(2,	1,	'Jakarta Selatan',	10000000),
-(3,	2,	'Kota Tangerang',	5000000),
-(4,	3,	'Bandung',	7000000),
-(5,	3,	'Cirebon',	4000000);
